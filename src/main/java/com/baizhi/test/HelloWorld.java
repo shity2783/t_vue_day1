79fa852369187944b429e674e9e53b399e2fcf17
shity2783 <68982487+shity2783@users.noreply.github.com>
2020-08-11 13:01:30 +0800

    Update HelloWorld.java

--- src/main/java/com/baizhi/test/HelloWorld.java
+++ src/main/java/com/baizhi/test/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("哈哈哈2");
-        System.out.println("dev");
+        System.out.println("dev update in github");
+        
     }
 }
